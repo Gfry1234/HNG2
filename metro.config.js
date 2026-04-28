f54039994ba02af3913472f4e654bddf063a1662
Gfry1234 <iamgfry@yahoo.com>
2026-04-28 20:37:45 +0300

    minors in the game

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Resolve the @ alias to the project root
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName.startsWith('@/')) {
-    const relativePath = moduleName.slice(2);
-    const resolvedPath = path.resolve(context.originModuleDir, relativePath);
-    return context.resolveRequest(context, resolvedPath, platform);
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
 
 module.exports = config;
